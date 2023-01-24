38c4c085a4b1e4223759b6813477c50162074081
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-01-24 23:12:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // Pobierz dane z API
-  $.getJSON("https://yourAPIurl.com", function(data){
+  $.getJSON("https://raw.githubusercontent.com/LuckyMan612/Zombies-Infested/main/files/api.json", function(data){
     // Pobierz tylko pierwsze 3 ogłoszenia
     var announcements = data.slice(0,3);
     // Iteruj przez ogłoszenia i dodaj je do kontenera
