b93b91097df4f424d5d576799011acdd985cff64
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-01-25 12:27:16 +0100

    Update screenshooty.js

--- screenshooty.js
+++ screenshooty.js
@@ -1,8 +1,12 @@
 // Tworzymy tablicę ze zdjęciami
 var screenshots = [
-  "screenshot1.jpg",
-  "screenshot2.jpg",
-  "screenshot3.jpg"
+  "https://github.com/LuckyMan612/Zombies-Infested/blob/main/files/game/images/obraz_2023-01-25_121154054.png?raw=true",
+  "https://github.com/LuckyMan612/Zombies-Infested/blob/main/files/game/images/obraz_2023-01-25_122126331.png?raw=true",
+  "https://github.com/LuckyMan612/Zombies-Infested/blob/main/files/game/images/obraz_2023-01-25_122338010.png?raw=true",
+  "https://github.com/LuckyMan612/Zombies-Infested/blob/main/files/game/images/obraz_2023-01-25_122403069.png?raw=true",
+  "https://github.com/LuckyMan612/Zombies-Infested/blob/main/files/game/images/obraz_2023-01-25_122417257.png?raw=true",
+  "https://github.com/LuckyMan612/Zombies-Infested/blob/main/files/game/images/obraz_2023-01-25_122427677.png?raw=true",
+  "https://github.com/LuckyMan612/Zombies-Infested/blob/main/files/game/images/obraz_2023-01-25_122458297.png?raw=true"
 ];
 
 // Iterujemy przez tablicę i dodajemy zdjęcia do sekcji
